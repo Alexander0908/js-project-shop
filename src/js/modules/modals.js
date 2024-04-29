5c1543921045c37874c0cb5cdf0127a39c93b1c4
Alex <example@gmail.com>
2024-04-29 23:18:58 +0300

    first commit

---
+++ src/js/modules/modals.js
@@ -0,0 +1,32 @@
+const modals = () => {
+    function bindModal(trigger, modal, close) {
+        trigger.addEventListener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
+
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            }
+        })
+    }
+
+    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+          modalEngineer = document.querySelector('.popup_engineer'),
+          modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+        
+    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+};
+
+export default modals;
